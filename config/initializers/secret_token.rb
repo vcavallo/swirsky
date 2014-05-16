2fa6fcb9d88fe79d5dfccd54dfae1f234a99f1da
vcavallo <vcavallo@gmail.com>
2014-05-16 13:27:22 -0400

    Initial commit of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwirskyRails::Application.config.secret_key_base = '866cdd4360084c601b2aa419b374ba84e0edc3e1c5b8025e9a39b3c1c1e55cc2ced56dfeaac37beb546cc429c10f3e0927bf76e58a44f4a53d81811e3adb9ee2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwirskyRails::Application.config.session_store :cookie_store, key: '_swirsky_rails_session'
