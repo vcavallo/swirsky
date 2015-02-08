8d0c4dd0b9100257411ac0b5c42d0eebcd894ad2
vcavallo <vcavallo@gmail.com>
2015-02-08 17:56:44 -0500

    Removes old controller and route code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,8 @@ class PagesController < ApplicationController
     render 'pages/index'
   end
 
-  def meet_dr_mike
-    render 'pages/meet-dr-mike'
-  end
-
-  def services
-
-  end
-
   def show
     render "pages/#{params[:page]}"
-  end
-
-  def static_page
   end
 
 end
